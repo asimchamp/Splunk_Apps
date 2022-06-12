1e5407a87577281fce79b8f6b0984cfd6ff3d60c
asimchamp <61060339+asimchamp@users.noreply.github.com>
2022-06-13 03:11:27 +0530

    Update app_downloader.sh

--- app_downloader.sh
+++ app_downloader.sh
@@ -24,11 +24,11 @@ database_folder=$(ls | grep -c SplunkBase)
 if [[ "$database_folder" = 1 ]];
    then
       echo "Database folder present."
-      curl -L -J -v -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/2890/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print "Name = " $1}' > splunk_name.txt
-      curl -L -J -v -H "X-Auth-Token: $splunk_token"  https://splunkbase.splunk.com/app/2890/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print "version = " $2}' >> splunk_name.txt
+      curl -L -J -v -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/2890/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print "Name = " $1}' > /home/runner/work/Splunk_Apps/Splunk_Apps/SplunkBase/splunk_name.txt
+      curl -L -J -v -H "X-Auth-Token: $splunk_token"  https://splunkbase.splunk.com/app/2890/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print "version = " $2}' >> /home/runner/work/Splunk_Apps/Splunk_Apps/SplunkBase/splunk_name.txt
    else
       echo "Database Folder not Present."
       mkdir DataBase
-      curl -L -J -v -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/2890/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print "Name = " $1}' > splunk_name.txt
-      curl -L -J -v -H "X-Auth-Token: $splunk_token"  https://splunkbase.splunk.com/app/2890/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print "version = " $2}' >> splunk_name.txt
+      curl -L -J -v -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/2890/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print "Name = " $1}' > /home/runner/work/Splunk_Apps/Splunk_Apps/SplunkBase/splunk_name.txt
+      curl -L -J -v -H "X-Auth-Token: $splunk_token"  https://splunkbase.splunk.com/app/2890/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print "version = " $2}' >> /home/runner/work/Splunk_Apps/Splunk_Apps/SplunkBase/splunk_name.txt
 fi
