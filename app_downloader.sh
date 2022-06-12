6fc63eae9ac2da6b6b3a0b4c86dfda4a8fc9e980
asimchamp <61060339+asimchamp@users.noreply.github.com>
2022-06-13 03:00:50 +0530

    Update app_downloader.sh

--- app_downloader.sh
+++ app_downloader.sh
@@ -13,17 +13,19 @@ echo
 
 ################ Variables Sections ################
 
-splunk_token=$( cat SplunkBase/splunk_token.txt | awk '{print $3}' )
+splunk_token=$( cat splunk_token.txt | awk '{print $3}' )
 
 
 ################# Prepare the app details ###############
 
 # Checking the DataBase folder present or not
-database_folder=$(ls | grep -c Database)
+database_folder=$(ls | grep -c SplunkBase)
 
 if [[ "$database_folder" = 1 ]];
    then
       echo "Database folder present."
+      curl -L -J -v -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/2890/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print "Name = " $1}' > splunk_name.txt
+      curl -L -J -v -H "X-Auth-Token: $splunk_token"  https://splunkbase.splunk.com/app/2890/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print "version = " $2}' >> splunk_name.txt
    else
       echo "Database Folder not Present."
       mkdir DataBase
