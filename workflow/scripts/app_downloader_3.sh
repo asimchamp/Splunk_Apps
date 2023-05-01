ec80e28ed114ee549af5774f68eb374f8d74f2a6
asimchamp <61060339+asimchamp@users.noreply.github.com>
2023-05-01 23:43:13 +0530

    Update app_downloader_3.sh

--- workflow/scripts/app_downloader_3.sh
+++ workflow/scripts/app_downloader_3.sh
@@ -11,8 +11,10 @@ echo '################################################'
 echo
 echo
 
+
 ################ Variables Sections ################
 
+#splunk_home='/workspace/Splunk_installation'
 splunk_home='/home/runner/work/Splunk_Apps/Splunk_Apps'
 
 #splunk_token=$( cat /home/runner/work/Splunk_Apps/Splunk_Apps/splunk_token.txt | awk '{print $3}' )
@@ -20,7 +22,8 @@ splunk_token=$SPLUNK_TOKEN
 
 workflow_number='_3'
 
-# Checking the Workflow folder present or not
+### Checking the Workflow folder present or not
+
 workflow_folder=$(ls $splunk_home/ | grep -cw workflow)
 
 if [ "$workflow_folder" = "1" ];
@@ -34,28 +37,21 @@ if [ "$workflow_folder" = "1" ];
 
 fi
 
-# Checking the Workflow number folder present or not
+### Checking the Workflow number folder present or not
+
 workflow_number_folder=$(ls $splunk_home/workflow/ | grep -cw workflow$workflow_number)
 
 if [ "$workflow_number_folder" = "1" ];
    then
-      echo "workflow folder present."
-
+      echo "workflow folder number present."
+	  
    else
       echo "workflow Folder not Present."
       mkdir $splunk_home/workflow/workflow$workflow_number
       echo "workflow Folder Created."
+	  
 
 fi
-
-app_download_func ()
-{
-################# Prepare the app details ###############
-BASE_APP_NAME=$( cat $splunk_home/workflow/workflow$workflow_number/splunk_name.txt | grep $e | awk -F"=" '{print $3}' |  awk -F"," '{print $1}' |  sed "s/ $//g" | sed 's/ /_/g' )
-
-BASE_APP_VER=$( cat $splunk_home/workflow/workflow$workflow_number/splunk_name.txt | grep $e | awk -F"," '{print $2}' | awk -F"=" '{print $2}' )
-
-BASE_DOWN_URL='http://splunkbase.splunk.com/app/$e/release/$BASE_APP_VER/download/'
 
 # Checking the DataBase folder present or not
 SplunkBase_folder=$(ls $splunk_home/ | grep -cw SplunkBase)
@@ -70,6 +66,18 @@ if [ "$SplunkBase_folder" = "1" ];
       echo "SplunkBase Folder Created."
 
 fi
+
+
+
+app_download_func ()
+{
+################# Prepare the app details ###############
+BASE_APP_NAME=$( cat $splunk_home/workflow/workflow$workflow_number/splunk_name.txt | grep $e | awk -F"=" '{print $3}' |  awk -F"," '{print $1}' |  sed "s/ $//g" | sed 's/ /_/g' )
+
+BASE_APP_VER=$( cat $splunk_home/workflow/workflow$workflow_number/splunk_name.txt | grep $e | awk -F"," '{print $2}' | awk -F"=" '{print $2}' )
+
+BASE_DOWN_URL='http://splunkbase.splunk.com/app/$e/release/$BASE_APP_VER/download/'
+
 
 APP_folder=$(ls $splunk_home/SplunkBase/ | grep -cw $e"_"$BASE_APP_NAME)
 
@@ -97,6 +105,25 @@ if [ "$VER_folder" = "1" ];
 
 fi
 
+### excluding over 50MB file
+
+file_size=$(curl -s -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i | awk -F",\"release\":" '{print $2}' | awk -F"\"size\":" '{print $2}' | cut -d ',' -f 1)
+
+if [ "$file_size" -gt "50000000" ];
+   then
+      echo "file_size=$file_size" "File size too large."
+	  
+   else
+	  echo "file_size=$file_size" "File size normal."
+	  tar_file_func
+      
+fi  
+
+}
+
+tar_file_func()
+{
+
 ##### Downloading the app file from SplunkBase
 
 
@@ -107,12 +134,30 @@ if [ "$file_tgz" = "1" ];
       echo "File already present."
    else
       cd $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER/
-      curl -L -J -O -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$e/release/$BASE_APP_VER/download/
+      curl -s -L -J -O -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$e/release/$BASE_APP_VER/download/
       sleep 2
-      tar -xvzf $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER/*.tgz -C $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER
-      sleep 2
+	  tar_file_size
+
 fi
 
+}
+
+tar_file_size()
+{
+
+check_tar_size=$( find $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER/ -type f -name "*.tgz" -size +50M | wc -l )
+
+if [ "$check_tar_size" = "1" ];
+   then
+   echo "Tar file size more than 50MB, deleting file"
+   rm -rf $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER/*.tgz
+   
+   else
+      echo "Tar file size less than 50MB, unzip tar tile"
+      tar -xvzf $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER/*.tgz -C $splunk_home/SplunkBase/$e"_"$BASE_APP_NAME/$BASE_APP_VER
+      sleep 2
+
+fi
 }
 
 rm -rf $splunk_home/workflow/workflow$workflow_number/splunk_name.txt
@@ -120,66 +165,82 @@ rm -rf $splunk_home/workflow/workflow$workflow_number/splunk_name.txt
 previous_check_func()
 {
 
-previous_download_app=$(cat $splunk_home/workflow/workflow$workflow_number/merge_uniq.txt | grep $e | wc -l )
+previous_download_app=$(cat $splunk_home/workflow/workflow$workflow_number/null_app.txt | grep "$i" | wc -l )
 
 if [ "$previous_download_app" = "1" ];
    then
    echo "App Present on old data bsase"
+   echo "previous_download_app=$previous_download_app"
    
 else
-   app_not_found=$(curl -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i/ | grep "404 Error: Page not found" | awk -F">" '{print $2}' | awk -F"." '{print $1}' )
-   echo "ID=$i" "app_not_found="$app_not_found, >> $splunk_home/workflow/workflow$workflow_number/1_app_not_found.txt
-   cat $splunk_home/workflow/workflow$workflow_number/1_app_not_found.txt | grep "404 Error: Page not found" >> $splunk_home/workflow/workflow$workflow_number/merge.txt
-
-   app_archive=$(curl -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i/ | grep "This app has been archived" |  awk -F"." '{print $1}' | cut -c 13-38 )
-   echo "ID=$i" "app_not_found="$app_archive, >> $splunk_home/workflow/workflow$workflow_number/2_app_archive.txt
-   cat $splunk_home/workflow/workflow$workflow_number/2_app_archive.txt | grep "This app has been archived" >> $splunk_home/workflow/workflow$workflow_number/merge.txt
-
-   app_not_available=$(curl -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i/ | grep "This app is currently not available" | awk -F">" '{print $2}' | awk -F"." '{print $1}' )
-   echo "ID=$i" "app_not_found="$app_not_available, >> $splunk_home/workflow/workflow$workflow_number/3_app_not_available.txt
-   cat $splunk_home/workflow/workflow$workflow_number/3_app_not_available.txt | grep "This app is currently not available" >> $splunk_home/workflow/workflow$workflow_number/merge.txt
-
-   app_found=$(curl -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i/ | grep -e 'Splunkbase</title>' | awk -F">" '{print $2}' | awk -F"|" '{print $1}' )
-   app_version=$(curl -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i/ | grep 'sb-release-select u-for="download-modal" sb-selector="release-version" sb-target="' | sed -n '1p' | awk -F"=" '{print $4}' | awk -F"\"" '{print $2}' )
-   echo "ID=$i" "app_found="$app_found, "app_version="$app_version >> $splunk_home/workflow/workflow$workflow_number/splunk_name.txt
+   echo "Checking on Splunkbase Portal"
+   echo "previous_download_app=$previous_download_app"
+   check_splunkbase_func
 
 fi
+
+}
+
+check_splunkbase_func()
+{
+
+file_name_present=$(curl -s -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i | grep filename | wc -l)
+
+if [ "$file_name_present" = "1" ];
+   then
+   echo "file_name_present=$file_name_present" "ID=$i" "App available in splunkbase portal"
+   app_found=$(curl -s -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i |  awk -F",\"title\":\"" '{print $2}' |  awk -F"\"" '{print $1}' )
+   app_version=$(curl -s -H "X-Auth-Token: $splunk_token" https://splunkbase.splunk.com/app/$i |  awk -F",\"release\":" '{print $2}' | awk -F"," '{print $2}' | awk -F":" '{print $2}' | sed "s/\"//g" )
+   echo "ID=$i" "app_found="$app_found, "app_version="$app_version >> $splunk_home/workflow/workflow$workflow_number/splunk_name1.txt
+   
+   else
+   echo "file_name_present=$file_name_present" "ID=$i" "App is not available in splunkbase portal"
+   echo "ID=$i" "msg=Not in splunkbase" >> $splunk_home/workflow/workflow$workflow_number/null_app1.txt
+fi   
 
 }
 
 ########## Checking app avaibility on Splunk Base #################
 for i in $(cat $splunk_home/workflow/workflow$workflow_number/app_id.txt)
 do
+   echo "Performing app avaibility function"
    previous_check_func
 
 done
 
-sort $splunk_home/workflow/workflow$workflow_number/merge.txt | uniq -d >> $splunk_home/workflow/workflow$workflow_number/1merge_uniq.txt
-sort $splunk_home/workflow/workflow$workflow_number/1merge_uniq.txt > $splunk_home/workflow/workflow$workflow_number/merge_uniq.txt
+sort $splunk_home/workflow/workflow$workflow_number/splunk_name1.txt | uniq > $splunk_home/workflow/workflow$workflow_number/splunk_name.txt
+sort $splunk_home/workflow/workflow$workflow_number/null_app1.txt | uniq > $splunk_home/workflow/workflow$workflow_number/null_app.txt
 rm -rf $splunk_home/workflow/workflow$workflow_number/merge.txt $splunk_home/workflow/workflow$workflow_number/1merge_uniq.txt $splunk_home/workflow/workflow$workflow_number/*_app_*
-sed -i 's/404//g' $splunk_home/workflow/workflow$workflow_number/splunk_name.txt
 
 echo "Loop Completed for list"
 
+
 for e in $(cat $splunk_home/workflow/workflow$workflow_number/app_id.txt)
 do
-   download_app=$(cat $splunk_home/workflow/workflow$workflow_number/merge_uniq.txt | grep $e | wc -l )
-   download_archive_app=$(cat $splunk_home/workflow/workflow$workflow_number/merge_uniq.txt | grep archived | grep $e | wc -l )
+   download_app=$(cat $splunk_home/workflow/workflow$workflow_number/splunk_name.txt | grep $e | wc -l )
    
-   if [ "$download_archive_app" = "1" ];
+   if [ "$download_app" = "1" ];
       then
-      echo "ID=$e"  "App archived available for the Download."
-      app_download_func
-      
-   elif [ "$download_app" = "1" ];
-      then
-      echo "ID=$e"  "App not available for the Download."
-
-   else
       echo "ID=$e"  "App available for the Download."
       app_download_func
 
+   else
+	  echo "ID=$e"  "App not available for the Download."
+
    fi
+   
+ ## deleting the blank folder
+ delete_blank=$(ls $splunk_home/SplunkBase/ | grep -cw $e"_" )
+ 
+ if [ "$delete_blank" = "1" ];
+   then
+      echo "ID=$e"  "folder present for deleting."
+      rm -rf $splunk_home/SplunkBase/$e"_"
+
+   else
+      echo "ID=$e"  "Folder not Present for deleting."
+
+fi
 
 done
 
